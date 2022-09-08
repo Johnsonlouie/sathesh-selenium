2f730480352d784d6f1a12b540f13bb504d7d2c4
johnsonlouie <johnsonaroaro@gmail.com>
2022-09-08 19:55:19 +0530

    sathesh selenium

---
+++ src/com/test/First.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+
+public class First {
+			
+	public static void main(String[] args) {
+		System.setProperty("WebDriver.chromedriver.driver", "C:\\Users\\Admin\\eclipse-workspace\\Selenium\\driver\\chromedriver.exe");
+	
+	}
+	
+}
